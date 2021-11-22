faa0745e03ec37361c1a04cd306e994aa43fd706
Terrell-Yan <email@example.com936932230@qq.comgit config --global user.email email@example.com>
2021-11-22 15:10:17 +0800

    版本降低29.0.3

---
+++ app/src/main/java/com/example/jacksoket/ThradClintent.java
@@ -0,0 +1,18 @@
+package com.example.jacksoket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThradClintent extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+
+
+
+}
